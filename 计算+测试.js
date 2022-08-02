a2574e9d4a66882ede127af6be78ce9353c10fea
XianChen1996 <chenxianfio96@gmail.com>
2022-08-02 09:53:01 +0900

    js 0802 1

---
+++ 计算+测试.js
@@ -0,0 +1,15 @@
+//const PI = Math.PI
+
+//console.log(PI)
+//计算
+//console.log(Math.pow(3, 2))   
+
+//字符串连接
+let firstName = 'Asabeneh'
+let lastName = 'Yetayeh'
+let space = ' '
+let fullName = firstName + space + lastName; // concatenation, merging two string together.
+console.log(fullName);
+
+
+//CTRL+f5 NODE JS 调试
